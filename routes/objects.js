5984cca34409ea8a4f64d801d412d0a9102359ea
Pontus Andersson <ponteman@live.se>
2020-10-20 12:24:46 +0200

    User can sell objects
    User can now sell their objects if they have enough

--- routes/objects.js
+++ routes/objects.js
@@ -103,6 +103,95 @@ router.post("/buy/:name",
                     })
 
             }
+            res.json({
+                data: {
+                    status: 200,
+                    msg: `${amount} ${animalName} objects were bought.`
+                }
+            })
+        })
+    })
+});
+
+router.post("/sell/:name",
+    (req, res, next) => checkToken(req, res, next),
+    (req, res) => {
+    var animalName = req.params.name;
+    var amount = req.body.amount;
+    var email = req.body.email;
+    var oldBalance = 0;
+    var newBalance = 0;
+    var price = 0;
+
+    db.serialize(function() {
+        var enough = true;
+
+        db.get("SELECT Price FROM Objects WHERE Name = ?",
+        animalName,
+        (err, objectPrice) => {
+            price = objectPrice.Price;
+        })
+
+        db.get("SELECT ObjectAmount FROM UserObjects WHERE UserEmail = ? AND ObjectName = ?",
+        email,
+        animalName,
+        (err, objectInfo) => {
+            var newAmount = objectInfo.ObjectAmount - parseInt(amount);
+            if (newAmount > 0) {
+                // UPDATE if user already has these objects
+
+                db.run("UPDATE UserObjects SET ObjectAmount = ? WHERE UserEmail = ? AND ObjectName = ?",
+                newAmount,
+                email,
+                animalName,
+                    (err) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                    })
+            } else if (newAmount == 0) {
+                db.run("DELETE FROM UserObjects WHERE UserEmail = ? AND ObjectName = ?",
+                email,
+                animalName,
+                    (err) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                    })
+            } else {
+                // Not enough objects in inventory
+                res.json({
+                    data: {
+                        status: 400,
+                        msg: "Not enough objects in inventory."
+                    }
+                })
+                enough = false;
+            }
+        })
+
+        db.get("SELECT Balance FROM Users WHERE Email = ?",
+        email,
+        (err, userBalance) => {
+            if (enough) {
+                oldBalance = userBalance.Balance;
+                newBalance = oldBalance + (amount*price);
+
+                db.run("UPDATE Users SET Balance = ? WHERE Email = ?",
+                newBalance,
+                email,
+                    (err) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                    })
+                res.json({
+                    data: {
+                        status: 200,
+                        msg: `${amount} ${animalName} objects were sold.`
+                    }
+                })
+            }
         })
     })
 });
